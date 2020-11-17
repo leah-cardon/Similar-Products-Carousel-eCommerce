59528ef0035d565c02b1efe4e874680dc9372c0b
Leah Cardon <cardonleah@gmail.com>
2020-11-17 15:51:40 -0700

    Added sample hardcoded image to show structure of app

--- client/ProductPhoto.jsx
+++ client/ProductPhoto.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const ProductPhoto = (props) => {
 
   return (
-    <div>will render main product photo</div>
+    <div>
+      <img src="https://images.unsplash.com/photo-1585652757141-8837d676fac8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"></img>
+    </div>
   );
 
 };
