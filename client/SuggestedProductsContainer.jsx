6b72a5eef3b4a843924ff6faa024360a3e381f2c
Leah Cardon <cardonleah@gmail.com>
2020-11-16 19:30:01 -0700

    Added files for React components and started them all off with boilerplate code.

---
+++ client/SuggestedProductsContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SuggestedProductsContainer = (props) => {
+  return (<div>'suggested products container'</div>);
+};
+
+
+
+export default SuggestedProductsContainer;
--- client/index.jsx
+++ client/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SuggestedProductsContainer from './SuggestedProductsContainer.jsx';
+
 
 class App extends React.Component {
-  render() {
-    return <div>Hello World</div>;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+    // bind methods here
   }
+
+  render() {
+    return <div><SuggestedProductsContainer /></div>;
+  }
+
 }
 
 
