6b18df67bda8af0cb4924c41873e51b1219cfe31
Leah Cardon <cardonleah@gmail.com>
2020-11-18 21:02:51 -0700

    Added basic formatting to SuggestedProductDetails component to more closely mirror the Sephora carousel's styling. Added a currency formatter.

--- client/SuggestedProductDetails.jsx
+++ client/SuggestedProductDetails.jsx
@@ -2,13 +2,29 @@ import React from 'react';
 
 const SuggestedProductDetails = (props) => {
 
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  });
+
   return (
     <div>
-      suggested product details
-      {props.details.brand}
-      {props.details.product_name}
-      {props.details.price}
-      {props.details.stars}
+      <div className='productHeader'>
+        <p class='brandName'>
+          {props.details.brand}
+        </p>
+        <p>
+          {props.details.product_name}
+        </p>
+      </div>
+      <div>
+        {formatter.format(props.details.price)}
+      </div>
+      <div>
+        {props.details.stars}
+        {props.details.reviews}
+      </div>
     </div>
   );
 
