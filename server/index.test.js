062fb2fd3e630dfd39219a48afb610c83c7032b1
Leah Cardon <cardonleah@gmail.com>
2020-11-19 21:28:30 -0700

    Fixed typo

--- server/index.test.js
+++ server/index.test.js
@@ -1,6 +1,6 @@
 const server = require('./index.js');
 const supertest = require('supertest');
-const request = supertest(app);
+const request = supertest(server);
 
 it('async test', async done => {
   // async tests here
