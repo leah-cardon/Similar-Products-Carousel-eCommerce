324e33dcce0ac01e57790afb7718e7ca348c1d89
Leah Cardon <cardonleah@gmail.com>
2020-11-19 20:17:45 -0700

    Fixed babel presets (I think)

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', '@babel/preset-react'],
-    ['@babel/present-env', {targets: {node: 'current'}}]
+    ['@babel/preset-env', {targets: {node: 'current'}}, '@babel/preset-react']
   ]
 };
---
+++ server/index.test.js
@@ -0,0 +1,5 @@
+const server = require('./index.js');
+
+test('tests 0 === 0', () => {
+  expect(0).toBe(0);
+});
