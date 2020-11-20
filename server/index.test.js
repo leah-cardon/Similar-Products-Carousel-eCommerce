7ad2f915b15a8d173f3a0e2b054370973a94da39
Leah Cardon <cardonleah@gmail.com>
2020-11-19 21:45:45 -0700

    Wrote a test for server endpoint to get all products

--- server/index.test.js
+++ server/index.test.js
@@ -2,7 +2,11 @@ const server = require('./index.js');
 const supertest = require('supertest');
 const request = supertest(server);
 
-it('async test', async done => {
+it('gets all similar products', async done => {
   // async tests here
+  // change to '/api/products/:id/similar'
+  const response = await request.get('/api/products/similar');
+  expect(response.status).toBe(200);
+  expect(response.body.length).toBe(100);
   done();
 });
