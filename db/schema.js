18b018f133a47b0ae63141eb583a62590062856a
Leah Cardon <cardonleah@gmail.com>
2020-11-20 21:16:56 -0700

    Figured out how to access the correct array of similar products and their info from the current ID

--- db/schema.js
+++ db/schema.js
@@ -66,8 +66,8 @@ const getOneProductInfo = (id) => {
 const getSuggestedProducts = (productId, type) => {
   return Products.find({'id': [productId]})
     .then(currentProduct => {
-      console.log('currentProduct: ', currentProduct);
-      return Products.find({ id: { $in: currentProduct[type]}});
+      console.log('currentProduct at similar: ', currentProduct[0]['similar']);
+      return Products.find({ id: { $in: currentProduct[0][type]}});
     })
     .catch((err) => {
       console.log('error in getSuggestedProducts: ', err);
