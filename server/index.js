ece575f619c98a9f750964e9224919d5185c4b13
Leah Cardon <cardonleah@gmail.com>
2020-11-29 20:40:07 -0700

    Added /*/:id route to server

--- server/index.js
+++ server/index.js
@@ -27,83 +27,26 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.get('/api/products/:id/similar', (req, res) => {
   const id = (req.params.id === undefined) ? 2 : req.params.id;
   db.getSuggestedProducts(id, 'similar')
-    .then((products) => {
-      res.status(200).send(products);
-    }).catch((err) => {
-      res.status(404).send(err);
-    });
+  .then((products) => {
+    res.status(200).send(products);
+  }).catch((err) => {
+    res.status(404).send(err);
+  });
 });
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/index.html'))
-// });
-
-app.get('/api/products/:id/youmayalsolike', (req, res) => {
-  const id = (req.params.id === undefined) ? 2 : req.params.id;
-  db.getSuggestedProducts(id, 'you_may_like')
-    .then((products) => {
-      res.status(200).send(products);
-    }).catch((err) => {
-      res.status(404).send(err);
-    });
-});
-
-
-
-// Add product to user's "Loves" list (favorites) (disregard complexity of users for now - just add a 'shopping list' table and join with products table?)
-// app.post('/api/shopping_list', (req, res) => {
-//   db.addToLoves(//req.body.id)
-//     .then((product) => {
-//       res.send(product);
+// app.get('/api/products/:id/youmayalsolike', (req, res) => {
+//   const id = (req.params.id === undefined) ? 2 : req.params.id;
+//   db.getSuggestedProducts(id, 'you_may_like')
+//     .then((products) => {
+//       res.status(200).send(products);
 //     }).catch((err) => {
 //       res.status(404).send(err);
 //     });
 // });
 
-// Parameters
-
-// id of product: integer
-// * Required
-// * The id of the "loved" product
-
-
-// Returns
-// Nothing
-
-// Status codes
-// * 201: Product successfully inserted into user's "Loves" list
-// * 400: Unsuccessful attempt at inserting new product into "Loves" list
-
-
-// Add product to user's cart
-
-// POST /api/basket
-
-// Parameters
-
-// product_id: integer
-// * Required
-// * The id of the "loved" product
-
-// size: string
-// * Required only if there are multiple sizes
-// * The desired size of product (only applies to some products)
-
-// color: string
-// * Required only if there are multiple colors
-// * The desired color of product (only applies to some products)
-
-// user_id: integer
-// * Optional
-// * The id of the current user, in order to save product to their cart indefinitely if they leave the session
-
-
-// Returns
-// Nothing
-
-// Status codes
-// * 201: Product successfully added to user's cart
-// * 400: Unsuccessful attempt at adding product to cart
+app.get('/*/:id', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+});
 
 app.listen(4444, () => {
   console.log('App listening at http://localhost:4444');
